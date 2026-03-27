312e02d6b7765164a7d4cc11517650246dbf5265
wuhao <wuhao@astrokoala.com>
2026-03-27 13:46:48 +0800

    更新 data-loader：移除摩尔线程，添加京东云

--- js/data-loader.js
+++ js/data-loader.js
@@ -47,7 +47,7 @@ export class DataLoader {
       'tencent-coding-plan.json',
       'aliyun-bailian-coding-plan.json',
       'infini-ai-coding-plan.json',
-      'mthreads-coding-plan.json'
+      'jdcloud-coding-plan.json'
     ];
   }
 
